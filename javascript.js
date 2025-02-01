6e1180271675e602b4de7b133a963c5de45dff35
MBARUDI <mbarudi@hotmail.com>
2025-02-01 16:34:28 -0300

    html

---
+++ javascript.js
@@ -0,0 +1,41 @@
+var a 
+let b 
+const c = 10
+
+a = 10
+b = 20
+
+console.log(a + b + c)
+
+//tipos de dados
+//string
+let nome = 'Rafael'
+console.log(nome)
+//number
+let idade = 30
+console.log(idade)
+//boolean
+let possuiFaculdade = true
+console.log(possuiFaculdade)
+//array
+let time = ['Flamengo', 'Vasco', 'Botafogo']
+console.log(time[0])
+//object
+let carro = {
+    marca: 'Ford',
+    ano: '2020'
+}
+console.log(carro.marca)
+//function
+function somar(a, b) {
+    return a + b
+}
+console.log(somar(5, 5))
+console.log(somar(10, 5))
+
+const pessoa = {
+    nome: 'Rafael',
+    idade: 30
+}   
+console.log(pessoa.nome)
+console.log(pessoa.idade)
